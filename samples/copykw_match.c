4bb50bc188460abeab4c648083fcc09db28d11af
billowkiller <billowkiller@gmail.com>
2013-10-13 02:55:31 -0400

    copy monitor

--- samples/copykw_match.c
+++ samples/copykw_match.c
@@ -1,35 +1,10 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <glib.h>
 #define MAXWORD 30
 
-
-typedef struct GSList{
-   struct GSList* next;
-   char* data;
-}GSList;
-
 GSList *g_kw_list = NULL;
-
-GSList* current_kw=NULL;
-
-GSList* g_list_append(GSList* kw_list,char* word)
-{
-    if(kw_list==NULL)
-    {
-        kw_list=(GSList*)malloc(sizeof(GSList));
-        kw_list->data=word;
-        current_kw=kw_list;
-    }
-    else{
-        GSList* pre=current_kw;
-        current_kw=(GSList*)malloc(sizeof(GSList));
-        current_kw->data=word;
-        pre->next=current_kw;
-    }
-    current_kw->next=NULL;
-    return kw_list;
-}
 
 void read_kw_file(char *filename)
 {
@@ -37,7 +12,7 @@ void read_kw_file(char *filename)
 	while(1)
 	{
 		char *word = (char *)malloc(MAXWORD);
-		//int n = fscanf(file, "%s", word);
+		//int n = fscanf(file, "%s ", word);
         fgets(word,MAXWORD,file);
         word[strlen(word)-1]='\0';
         int n=strlen(word);
@@ -71,17 +46,15 @@ char* fileRead(char *filename)
 	return data;
 }
 
-int kw_match(char *str, GSList *list)
+int kw_match(char *str)
 {
 	GSList *iterator = NULL;
-	for (iterator = list; iterator; iterator = iterator->next)
+	for (iterator = g_kw_list; iterator; iterator = iterator->next)
 	{
 		 if(strstr(str, (char*)iterator->data))
 		 {
-		     //printf("%s\n",iterator->data);
 			 return 1;
 		 }
 	}
 	return 0;
 }
-
